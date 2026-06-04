628d30cc7437706cc4a43bdead484812f25b74d3
ArtinaxW <161110817+BuiThanhNghiaDTU19122004@users.noreply.github.com>
2026-06-04 15:18:29 +0700

    feat: terraform for day-a

--- cloud/w8/BE/terraform/main.tf
+++ cloud/w8/BE/terraform/main.tf
@@ -64,3 +64,60 @@ resource "aws_dynamodb_table" "ehr" {
 
   tags = local.common_tags
 }
+
+resource "aws_sqs_queue" "annotation_dlq" {
+  name = "${var.project_name}-${var.environment}-annotation-dlq"
+  tags = local.common_tags
+}
+
+resource "aws_sqs_queue" "annotation" {
+  name                       = "${var.project_name}-${var.environment}-annotation-queue"
+  visibility_timeout_seconds = 180
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.annotation_dlq.arn
+    maxReceiveCount     = 3
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_cloudwatch_event_rule" "s3_object_created" {
+  name        = "${var.project_name}-${var.environment}-documents-created"
+  description = "Route S3 ObjectCreated documents/* events to SQS"
+
+  event_pattern = jsonencode({
+    source        = ["aws.s3"]
+    "detail-type" = ["Object Created"]
+    detail = {
+      bucket = { name = [aws_s3_bucket.documents.bucket] }
+      object = { key = [{ prefix = "documents/" }] }
+    }
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_cloudwatch_event_target" "annotation_queue" {
+  rule = aws_cloudwatch_event_rule.s3_object_created.name
+  arn  = aws_sqs_queue.annotation.arn
+}
+
+resource "aws_sqs_queue_policy" "annotation_events" {
+  queue_url = aws_sqs_queue.annotation.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "events.amazonaws.com" }
+      Action    = "sqs:SendMessage"
+      Resource  = aws_sqs_queue.annotation.arn
+      Condition = {
+        ArnEquals = {
+          "aws:SourceArn" = aws_cloudwatch_event_rule.s3_object_created.arn
+        }
+      }
+    }]
+  })
+}
